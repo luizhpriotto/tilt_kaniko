a58e329c5d70b9794c200d482ab9ba17d0f30d78
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-03-26 15:24:25 -0300

    ok

--- tiltstart.sh
+++ tiltstart.sh
@@ -1,6 +1,6 @@
 sed -e "s/\${tag}/tilt-$(git rev-parse --abbrev-ref HEAD)/" docker-compose-template.yml > docker-compose.yml
 sed -e "s/\${tag}/tilt-$(git rev-parse --abbrev-ref HEAD)/" Tiltfile-template > Tiltfile
-sudo usermod -a -G docker $USER
+#sudo usermod -a -G docker $USER
 if [ ! -f ./tilt ]; then
    curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
 fi
